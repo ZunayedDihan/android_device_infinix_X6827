5900d9048ca9162fce0ea42f1e60feff05589227
Hotorou <37245252+ZunayedDihan@users.noreply.github.com>
2023-09-20 22:55:54 +0600

    disable these to reduce twrp size(boot)
    i cry with 
    /bin/bash -c "(out/host/linux-x86/bin/mkbootimg --kernel out/target/product/X6827/kernel --ramdisk out/target/product/X6827/ramdisk-recovery.img  --cmdline \"bootopt=64S3,32N2,64N2 twrpfastboot=1 buildvariant=eng\" --base 0x40078000 --pagesize 2048 --os_version 99.87.36 --os_patch_level 2099-12-31  --ramdisk_offset 0x07c08000 --tags_offset 0x0bc08000 --dtb device/infinix/X6827/prebuilt/dtb.img --header_version 2 --output  out/target/product/X6827/boot.img ) && (size=\$(for i in  out/target/product/X6827/boot.img; do stat -c \"%s\" \"\$i\" | tr -d '\\n'; echo +; done; echo 0); total=\$(( \$( echo \"\$size\" ) )); printname=\$(echo -n \" out/target/product/X6827/boot.img\" | tr \" \" +); maxsize=\$((     33554432-69632)); if [ \"\$total\" -gt \"\$maxsize\" ]; then echo \"error: \$printname too large (\$total > \$maxsize)\"; false; elif [ \"\$total\" -gt \$((maxsize - 32768)) ]; then echo \"WARNING: \$printname approaching size limit (\$total now; limit \$maxsize)\"; fi ) && (out/host/linux-x86/bin/avbtool add_hash_footer --image  out/target/product/X6827/boot.img --partition_size   33554432 --partition_name boot  --prop com.android.build.boot.fingerprint:\$(cat out/target/product/X6827/build_fingerprint.txt) --prop com.android.build.boot.os_version:99.87.36 )"
    error: +out/target/product/X6827/boot.img too large (38758400 > 33484800)
    ninja: build stopped: subcommand failed.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -168,5 +168,5 @@ endif
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/recovery/root/system/etc/recovery.fstab
 
 # Debug
-TWRP_INCLUDE_LOGCAT := true
-TARGET_USES_LOGD := true
+#TWRP_INCLUDE_LOGCAT := true
+#TARGET_USES_LOGD := true
