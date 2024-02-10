1bdbf822a74385325428dbb95ec0ffa9559a00cf
shazu-xd <shazuxd@proton.me>
2024-02-10 21:09:09 +0530

    x6827: drop crypto stuff
    Signed-off-by: shazu-xd <shazuxd@proton.me>

--- device.mk
+++ device.mk
@@ -57,34 +57,34 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.1-mtkimpl.recovery \
     android.hardware.boot@1.1-mtkimpl
 
-PRODUCT_PACKAGES_DEBUG += \
-    bootctrl
+#PRODUCT_PACKAGES_DEBUG += \
+#    bootctrl
 
-PRODUCT_PACKAGES += \
-    bootctrl.mt6781 \
-    bootctrl.mt6781.recovery
+#PRODUCT_PACKAGES += \
+#    bootctrl.mt6781 \
+#    bootctrl.mt6781.recovery
 
 # Fastbootd
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock \
     fastbootd
 
-PRODUCT_PACKAGES_DEBUG += \
-    update_engine_client
+#PRODUCT_PACKAGES_DEBUG += \
+ #   update_engine_client
 
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    cppreopts.sh \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
+#PRODUCT_PACKAGES += \
+#    otapreopt_script \
+#    cppreopts.sh \
+#    update_engine \
+#    update_verifier \
+#    update_engine_sideload
 	
 # Recovery modules
-TARGET_RECOVERY_DEVICE_MODULES += \
-    libkeymaster4 \
-    libpuresoftkeymasterdevice
+#TARGET_RECOVERY_DEVICE_MODULES += \
+#    libkeymaster4 \
+#    libpuresoftkeymasterdevice
 
-RECOVERY_LIBRARY_SOURCE_FILES += \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+#RECOVERY_LIBRARY_SOURCE_FILES += \
+#    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+#    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
