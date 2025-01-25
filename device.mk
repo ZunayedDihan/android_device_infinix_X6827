11f0ef48b7aac0f66527e515ec5cf5796e19cae8
root <root@Hotorou.localdomain>
2025-01-25 23:18:53 +0600

    Revert "x6827: drop crypto stuff"
    This reverts commit 1bdbf822a74385325428dbb95ec0ffa9559a00cf.

--- device.mk
+++ device.mk
@@ -80,11 +80,11 @@ PRODUCT_PACKAGES += \
     update_engine_sideload
 	
 # Recovery modules
-#TARGET_RECOVERY_DEVICE_MODULES += \
-#    libkeymaster4 \
-#    libpuresoftkeymasterdevice
+TARGET_RECOVERY_DEVICE_MODULES += \
+    libkeymaster4 \
+    libpuresoftkeymasterdevice
 
-#RECOVERY_LIBRARY_SOURCE_FILES += \
-#    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
-#    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
+RECOVERY_LIBRARY_SOURCE_FILES += \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libkeymaster4.so \
+    $(TARGET_OUT_SHARED_LIBRARIES)/libpuresoftkeymasterdevice.so
 
